7fe5f1d61f00afba6ce3e6152a755c152f4db6e7
aruna <arunadegala18@gmail.com>
2025-02-18 12:19:13 +0530

    variables

---
+++ shell_script/variablesconfidential.sh
@@ -0,0 +1,7 @@
+#for credintials not showing on terminal
+#!/bin/bash
+echo "enter ur username"
+read USERNAME
+echo "enter the password"
+read PASSWORD
+echo "The username and password is:$USERNAME,$PASSWORD"
