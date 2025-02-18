f338096f2aaa8d08f6afb1bb65c7a960cfd47d94
aruna <arunadegala18@gmail.com>
2025-02-18 19:27:56 +0530

    conditions

--- shell_script/sqlinstall.sh
+++ shell_script/sqlinstall.sh
@@ -3,7 +3,8 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
    echo "ERROR:: Plz use root user"
+   exit 1
    else
        echo "u r a root user"
        fi
-       yum install sql -y
+ yum install sql -y
