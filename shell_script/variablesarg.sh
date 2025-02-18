b17a0066d15b50754c54ee0a4e2eed89ae5ee83c
aruna <arunadegala18@gmail.com>
2025-02-18 12:05:34 +0530

    variablesarg

---
+++ shell_script/variablesarg.sh
@@ -0,0 +1,9 @@
+#arguements=arg
+#we no need to change the variable name every time ,we can give it in command line
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:Hi $PERSON2,how r u?"
+echo "$PERSON2:i am good $PERSON1,how r u?"
+echo "$PERSON1: i am good $PERSON2"
