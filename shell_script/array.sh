38c36e2ed269f7c7e9bd241eaddaf2d9abb64ea2
aruna <arunadegala18@gmail.com>
2025-02-18 13:38:06 +0530

    variables

---
+++ shell_script/array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FRUITS=("apple" "banana" "orange")
+echo "The first fruit is: ${FRUITS[0]}"
+echo "The second fruit is: ${FRUITS[1]}"
+echo "The third fruit is: ${FRUITS[2]}"
+echo "The number of fruit are: ${FRUITS[@]}"
+echo "The total number of fruit are: ${FRUITS[$]}"
