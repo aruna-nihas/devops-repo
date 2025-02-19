9dd2204ddaaa61b47a4b7e8808cf1769c9b68d92
aruna <arunadegala18@gmail.com>
2025-02-19 12:09:41 +0530

    shell-script

---
+++ shell_script/colors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+    echo -e "\e[31m ERROR::e[0m plz use root user"
+    exit 1
+    else
+        echo -e "e\[32m u r a root user"
+fi
+yum install git -y
+if [ $? -ne 0 ]
+then 
+    echo -e "\e[31m git installation is failed...."
+    else 
+       echo -e "\e[32m git installation is successful...."
+fi
