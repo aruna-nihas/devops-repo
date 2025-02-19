027f5d803fe6fd173f591157ad8c00e2282e0ece
aruna <arunadegala18@gmail.com>
2025-02-19 11:14:29 +0530

    shell-script

--- shell_script/array.sh
+++ shell_script/array.sh
@@ -3,6 +3,6 @@ FRUITS=("apple" "banana" "orange")
 echo "The first fruit is: ${FRUITS[0]}"
 echo "The second fruit is: ${FRUITS[1]}"
 echo "The third fruit is: ${FRUITS[2]}"
-echo "The number of fruit are: ${FRUITS[@]}"
-echo "Total number of fruit are: ${FRUITS[#]}"
+echo " all fruit are: ${FRUITS[@]}"
+echo "Total number of fruit are: ${FRUITS[$#]}"
 
