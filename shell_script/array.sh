f8cb47b59df81533d6c3a1150bde3c746dcdc56c
aruna <arunadegala18@gmail.com>
2025-02-19 11:16:08 +0530

    shell-script

--- shell_script/array.sh
+++ shell_script/array.sh
@@ -4,5 +4,5 @@ echo "The first fruit is: ${FRUITS[0]}"
 echo "The second fruit is: ${FRUITS[1]}"
 echo "The third fruit is: ${FRUITS[2]}"
 echo " all fruit are: ${FRUITS[@]}"
-echo "Total number of fruit are: ${FRUITS[$#]}"
+
 
