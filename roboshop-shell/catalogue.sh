31bffea79801c7dad1ff181d56756ce1c9add43b
aruna <arunadegala18@gmail.com>
2025-03-30 11:47:29 +0530

    roboshop-shell

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -8,7 +8,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo -e "$G The script started at $TIMESTAMP $N"
 
-VALIDATE(){
+VALIDATE () {
     if [ $1 -ne 0 ]
     then 
         echo -e "$2....$R Failed $N"
