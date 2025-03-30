f233250c6c4e1455310f4adb8721644e47709d02
aruna <arunadegala18@gmail.com>
2025-03-30 11:08:58 +0530

    roboshop-shell

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -4,6 +4,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+TIMESTAMP=$(date +F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo -e "$G The script started at $TIMESTAMP $N"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -22,20 +25,20 @@ then
 fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "mongodb installation"
 
-systemctl enable mongod
+systemctl enable mongod &>> $LOGFILE
 
 VALIDATE $? "enabling mongodb"
 
-systemctl start mongod
+systemctl start mongod &>> $LOGFILE
 
 VALIDATE $? "starting mongodb"
 
-sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 
-systemctl restart mongod
+systemctl restart mongod &>> $LOGFILE
 
 VALIDATE $? "restarting mongodb"
