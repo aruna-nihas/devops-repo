5800c3ca9981c0ee83a2cdfb8eb40a566a2ac537
aruna <arunadegala18@gmail.com>
2025-03-30 10:20:51 +0530

    roboshop shell

---
+++ roboshop-shell/mongodb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+    echo"ERROR....Plz use root user"
+    exit 1
+    else
+        echo "u r a root user"
+fi
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+
