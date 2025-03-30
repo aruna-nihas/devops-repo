d16b25e5c7a0fcf43aa1b4e7c04e39f716a771f5
aruna <arunadegala18@gmail.com>
2025-03-30 10:49:42 +0530

    roboshop-shell

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ID=$(id -u)
 if [ $ID -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2....Failed"
+        else
+            echo "$2....successful"
+    fi
+}
 then
     echo "ERROR....Plz use root user"
     exit 1
@@ -11,10 +19,18 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y 
 
+VALIDATE $? "mongodb installation"
+
 systemctl enable mongod
 
+VALIDATE $? "enabling mongodb"
+
 systemctl start mongod
+
+VALIDATE $? "starting mongodb"
 
 sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
 systemctl restart mongod
+
+VALIDATE $? "restarting mongodb"
