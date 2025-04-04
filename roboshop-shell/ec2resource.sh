9e6de46d0e546f68bff3f4fadabf27c2bfadbb20
aruna <arunadegala18@gmail.com>
2025-04-04 19:52:45 +0530

    ec2access

--- roboshop-shell/ec2resource.sh
+++ roboshop-shell/ec2resource.sh
@@ -5,11 +5,13 @@ INSTANCES=("mongodb" "mysql" "rabbitmq" "redis" "catalogue" "user" "cart" "web" 
 
 for i in "${INSTANCES[@]}"
 do
-  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "redis" ] || [$i == rabbitmq ]
+  echo "Instance Name:$i" 
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "redis" ] || [ $i == "rabbitmq" ]
   then
       INSTANCES_TYPE="t2.midium"
       else
       INSTANCES_TYPE="t2.micro"
+      echo "$i instance is created"
    fi
       aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG_ID
 done
